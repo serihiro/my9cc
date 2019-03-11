af278ae86669564cb443eb42df4972f5cfa89c1a
Kazuhiro Serizawa <nserihiro@gmail.com>
2019-03-11 08:45:36 +0000

    implement LL(1) parser

--- 9cc.c
+++ 9cc.c
@@ -8,13 +8,108 @@ enum {
     TK_EOF,
 };
 
+enum {
+    ND_NUM = 256,
+};
+
 typedef struct {
     int ty;
     int val;
     char *input;
 } Token;
 
+typedef struct Node {
+    int ty;
+    struct Node *lhs;
+    struct Node *rhs;
+    int val;
+} Node;
+
 Token tokens[100];
+int pos;
+
+void error(char *message, char *input);
+int consume(int ty);
+void tokenize(char *p);
+void gen(Node *node);
+Node *new_node(int ty, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *add();
+Node *mul();
+Node *term();
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = ty;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = malloc(sizeof(Node));
+    node->ty = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+int consume(int ty) {
+    if (tokens[pos].ty != ty) {
+        return 0;
+    }
+    ++pos;
+    return 1;
+}
+
+Node *term() {
+    if (tokens[pos].ty == TK_NUM) {
+        return new_node_num(tokens[pos++].val);
+    } 
+
+    return NULL;
+}
+
+Node *mul() {
+    return term();
+}
+
+Node *add() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node('+', node, mul());
+        } else if (consume('-')) {
+            node = new_node('-', node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+void gen(Node *node) {
+    if (node->ty == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->ty) {
+        case '+':
+            printf("  add rax, rdi\n");
+            break;
+        case '-':
+            printf("  sub rax, rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
 
 void tokenize(char *p) {
     int i=0;
@@ -40,17 +135,15 @@ void tokenize(char *p) {
             continue;
         }
 
-        fprintf(stderr, "トークナイズできません: %s\n", p);
-        exit(1);
+        error("トークナイズできません: %s\n", p);
     }
 
     tokens[i].ty = TK_EOF;
     tokens[i].input = p;
 }
 
-void error(int i) {
-    fprintf(stderr, "予期しないトークンです: %s\n",
-            tokens[i].input);
+void error(char *message, char *input) {
+    fprintf(stderr, message, input);
     exit(1);
 }
 
@@ -61,42 +154,15 @@ int main(int argc, char **argv) {
     }
 
     tokenize(argv[1]);
+    Node *node = add();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
  
-    if (tokens[0].ty != TK_NUM) {
-        error(0);
-    }
-    printf("  mov rax, %d\n", tokens[0].val);
-    
-    int i = 1;
-    while (tokens[i].ty != TK_EOF) {
-        if (tokens[i].ty == '+') {
-            ++i;
-            if (tokens[i].ty != TK_NUM) {
-                error(i);
-            }
-            printf("  add rax, %d\n", tokens[i].val);
-            ++i;
-            continue;
-        }
+    gen(node);
 
-        if (tokens[i].ty == '-') {
-            ++i;
-            if (tokens[i].ty != TK_NUM) {
-                error(i);
-            }
-            printf("  sub rax, %d\n", tokens[i].val);
-            ++i;
-            continue;
-        }
-
-        error(i);
-    }
-    
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
-
